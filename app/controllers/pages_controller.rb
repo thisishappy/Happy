b7420f2022d68bf144ca383d1510b42911a2c56e
ricburton <richard@thisishappy.com>
2010-11-28 18:59:11 -0800

    refresh first

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,10 @@
+class PagesController < ApplicationController
+  def home
+  end
+  #in ruby, the method will do nothing but in rails the contact method fetches the corresponding view
+
+  def contact
+  end
+  
+
+end
